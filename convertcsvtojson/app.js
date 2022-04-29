ddb024b7ebf8bc581803ae0f14e58541ac1fd593
kruvv <kruvv@mail.ru>
2022-04-29 16:44:17 +0300

    Добавил  парсер csv файлов и комментарии

--- convertcsvtojson/app.js
+++ convertcsvtojson/app.js
@@ -1,16 +1,37 @@
+/**
+ * Дата справочника: 2022-05-01
+ * Справочник информации о городах России находится по https://github.com/hflabs/city.git
+ * 
+ */
+
 const { time, timeEnd, log } = require("console")
 const fs = require("fs") 
 const path = require('path')
+const csv = require('csv-parser')
 
-path.dirname('/home/kruvv/vtcservice/vtc-test/copytest/')
+path.dirname('convertcsvtojson')
 
-// Чтение файла
+const inputFile = '/city-master/city.csv'
+const outNameFile = 'fullcitycoords.json'
+
 time('Read file')
-fs.readFile(`${__dirname}/fullcities.json`, 'utf8', (err, data) => {
-    if (err) throw err
 
-    const cities = JSON.parse(data)
+const cities = []
 
+// Читаем и парсим файл .csv
+fs.createReadStream(`${__dirname}${inputFile}`, 'utf8')
+.pipe(csv())
+.on('data', (data) => {        
+    const city = {
+        city: !data.city ? (!data.area ? data.region : data.area) : data.city,
+        region: data.region,
+        region_type: data.region_type,
+        geo_lat: +data.geo_lat,
+        geo_lon: +data.geo_lon
+    }
+    cities.push(city)
+})
+.on('end', () => {       
     let res = {}
     let dedupcount = {}
     let dedup = {}
@@ -18,7 +39,7 @@ fs.readFile(`${__dirname}/fullcities.json`, 'utf8', (err, data) => {
     for (const a of cities) {        
         for (const b of cities) {            
             if (a.city.toLowerCase() === b.city.toLowerCase() && a.region.toLowerCase() !== b.region.toLowerCase()) { 
-
+                // Подсчет дублей
                 dedupcount[a.city] ? dedupcount[a.city] += 1 : dedupcount[a.city] = 1 
 
                 const region_type = b.region_type === 'Респ' ? '' : b.region_type
@@ -29,14 +50,17 @@ fs.readFile(`${__dirname}/fullcities.json`, 'utf8', (err, data) => {
         res[a.city] = [a.geo_lat, a.geo_lon]    
     }
 
+    // Удаляем из общего списка города, которые имеют дубли
     for (const a of Object.keys(dedupcount)) {
         for (const b of Object.keys(res)) {
             if (a.toLowerCase() === b.toLowerCase()) delete res[b]
         }
     }
 
+    // Объединяем общий список городов с списком дублированных, но расположенных в разных регионах 
     const result = Object.assign(res, dedup)
 
+    // Сортировка городов по алфавиту
     Object.keys(result).sort().forEach(function(key) {
         let value = result[key]
         delete result[key]
@@ -45,7 +69,9 @@ fs.readFile(`${__dirname}/fullcities.json`, 'utf8', (err, data) => {
 
     writeResult(result)  
 })
+
 timeEnd('Read file')
+
 
 // Запись файла
 time('Write file')
@@ -53,9 +79,9 @@ function writeResult(coord) {
 
     let data = JSON.stringify(coord, null, '\t')
     
-    fs.writeFile(`${__dirname}/fullcitycoords.json`, data, err => {
+    fs.writeFile(`${__dirname}/out/${outNameFile}`, data, err => {
         if (err) throw err
-        log('Data written to file')
+        log(`Data written to file: ${outNameFile}`)
     })
 }
 timeEnd('Write file')
