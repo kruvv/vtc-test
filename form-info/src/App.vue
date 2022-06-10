39baeba8f837caccfa820fdc7220364503bfe6bf
kruvv <kruvv@mail.ru>
2022-06-10 16:56:26 +0300

    Создал приложение с формой ввода данных

---
+++ form-info/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>Введите данные для поиска</h1>
+    <Form/>
+  </div>
+</template>
+
+<script>
+import Form from './components/FormFirst.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Form
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ form-info/src/components/FormFirst.vue
@@ -0,0 +1,48 @@
+<template>
+    <form class="form-user">
+      <div class="mb-3">
+        <label for="lastName" class="form-label">Фамилия</label>
+        <input type="text" name="" class="form-control" id="lastName" />  
+      </div>
+       <div class="mb-3">
+        <label for="firstName" class="form-label">Имя</label>
+        <input type="text" name="" class="form-control" id="firstName" /> 
+        </div>
+         <div class="mb-3">
+        <label for="patronymic" class="form-label">Отчество</label>
+        <input type="text" name="" class="form-control" id="patronymic" /> 
+        </div>
+         <div class="mb-3">
+        <label for="birthday" class="form-label">Дата рождения</label>
+        <input type="date" name="" class="form-control" id="birthday" /> 
+        </div> 
+         <div class="mb-3">
+        <label for="inn" class="form-label">ИНН</label>
+        <input type="text" name="" class="form-control" id="inn" /> 
+        </div> 
+         <div class="mb-3">
+        <label for="passport" class="form-label">Номер и серию паспорта</label>
+        <input type="text" name="" class="form-control" id="passport" /> 
+        </div>
+        
+        <input type="submit" name="" class="btn btn-primary" value="Отправить" />
+    </form>    
+</template>
+
+<script>
+
+export default {
+  components: { }, 
+
+}
+</script>
+
+<style>
+  .form-user {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    max-width: 600px;
+  }
+
+</style>  
