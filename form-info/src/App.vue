37c2e7afc46062aba4ae2e9611d72b6412795d45
kruvv <kruvv@mail.ru>
2022-06-16 17:55:26 +0300

    Форматирование формы ввода

--- form-info/src/App.vue
+++ form-info/src/App.vue
@@ -21,8 +21,15 @@ export default {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
+    /* text-align: center; */
     color: #2c3e50;
     margin-top: 60px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;   
+  }
+
+  h1 {
+      margin-bottom: 40px;
   }
 </style>
--- form-info/src/components/FormFirst.vue
+++ form-info/src/components/FormFirst.vue
@@ -1,38 +1,45 @@
-<template>
-    <form class="form-user">
-      <div class="mb-3">
-        <label for="lastName" class="form-label">Фамилия</label>
-        <input type="text" name="" class="form-control" id="lastName" />  
-      </div>
-       <div class="mb-3">
-        <label for="firstName" class="form-label">Имя</label>
-        <input type="text" name="" class="form-control" id="firstName" /> 
-        </div>
-         <div class="mb-3">
-        <label for="patronymic" class="form-label">Отчество</label>
-        <input type="text" name="" class="form-control" id="patronymic" /> 
-        </div>
-         <div class="mb-3">
-        <label for="birthday" class="form-label">Дата рождения</label>
-        <input type="date" name="" class="form-control" id="birthday" /> 
-        </div> 
-         <div class="mb-3">
-        <label for="inn" class="form-label">ИНН</label>
-        <input type="text" name="" class="form-control" id="inn" /> 
-        </div> 
-         <div class="mb-3">
-        <label for="passport" class="form-label">Номер и серию паспорта</label>
-        <input type="text" name="" class="form-control" id="passport" /> 
-        </div>
+<template> 
+  <form class="form-user"  v-on:submit.prevent="sendData">
+    <div class="mb-3" v-for="field in fields" :key="field.id">
+      <label :for="field.id" class="form-label">{{field.name}}</label>
+      <input 
+        :type="field.type" 
+        :name="field.name" 
+        :id="field.id" 
+        :placeholder="field.placeholder"
+        :maxlenght="field.maxlenght" 
+        class="form-control" 
+      />  
+    </div>
+  <input type="submit" name="" class="btn btn-primary" value="Отправить" />
+  </form>
         
-        <input type="submit" name="" class="btn btn-primary" value="Отправить" />
-    </form>    
+      
 </template>
 
 <script>
 
 export default {
-  components: { }, 
+  components: { },
+  data () {
+    return {
+      fields: [
+        {id: 'lastName', type: 'text', name: 'Фамилия', placeholder: 'Иванов'},
+        {id: 'firstName', type: 'text', name: 'Имя', placeholder: 'Иван'},
+        {id: 'patronymic', type: 'text', name: 'Отчество', placeholder: 'Иванович'},
+        {id: 'birthday', type: 'date', name: 'Дата рождения'},
+        {id: 'inn', type: 'text', name: 'ИНН', placeholder: '774301390154', maxlength: 12},
+        {id: 'passport', type: 'text', name: 'Номер и серию паспорта', placeholder: '1236568799', maxlength: 10}
+      ]
+    }
+  },
+  methods: {
+      sendData: function () {
+        const form = document.getElementsByClassName('form-user')
+        // debugger
+        console.log('Hi', form);
+    } 
+  }
 
 }
 </script>
@@ -42,7 +49,7 @@ export default {
     display: flex;
     flex-direction: column;
     justify-content: center;
-    max-width: 600px;
+    min-width: 600px;
   }
 
 </style>  
